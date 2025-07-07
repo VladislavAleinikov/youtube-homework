c88c19688f5934d496fb2d191d4182ff114c3374
vladislav.aleynikov <55589606+VladAleinikov@users.noreply.github.com>
2025-07-07 22:53:19 +0300

    add video player page

---
+++ video-player-page/src/pages/video-player/ui/video-player-page.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { Header } from "../../../widgets/header";
+import styles from "./video-player-page.module.css";
+import { Recomended } from "../../../widgets/recomended";
+import { Video } from "../../../widgets/video";
+import { Sidebar } from "../../../widgets/sidebar";
+
+export const VideoPlayerPage = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const onToggleSidebar = () => setShowSidebar((prev) => !prev);
+
+  return (
+    <div className={styles["video-player-page"]}>
+      <Header onToggleSidebar={onToggleSidebar} />
+      <Sidebar show={showSidebar} />
+      <main className={styles.main}>
+        <Video />
+        <Recomended />
+      </main>
+    </div>
+  );
+};
