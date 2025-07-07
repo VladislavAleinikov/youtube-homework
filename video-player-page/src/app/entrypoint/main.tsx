4f70982dd1cdd7d9389248ee5f558a03c13e6a5d
vladislav.aleynikov <55589606+VladAleinikov@users.noreply.github.com>
2025-07-07 22:54:10 +0300

    remove unused icons

--- video-player-page/src/app/entrypoint/main.tsx
+++ video-player-page/src/app/entrypoint/main.tsx
@@ -1,8 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '../styles/index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "../styles/index.css";
+import { VideoPlayerPage } from "../../pages/video-player";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-  </StrictMode>,
-)
+    <VideoPlayerPage />
+  </StrictMode>
+);
