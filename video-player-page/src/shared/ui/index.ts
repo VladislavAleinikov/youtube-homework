17d685af13c6f28ffbfd3b8b27a93bdc0c459027
vladislav.aleynikov <55589606+VladAleinikov@users.noreply.github.com>
2025-07-07 22:48:19 +0300

    add title component

--- video-player-page/src/shared/ui/index.ts
+++ video-player-page/src/shared/ui/index.ts
@@ -1,5 +1,6 @@
 export { Button } from "./button";
 export { Avatar } from "./avatar";
+export { Title } from "./title";
 export { Burger } from "./icons/burger";
 export { Camera } from "./icons/camera";
 export { Dislike } from "./icons/dislike";
