c00b9cbf9fbaa10d71d753deade5fc841d01dae6
vladislav.aleynikov <55589606+VladAleinikov@users.noreply.github.com>
2025-07-07 22:52:04 +0300

    add header component

---
+++ video-player-page/src/widgets/header/ui/header.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styles from "./header.module.css";
+import {
+  Avatar,
+  Burger,
+  Button,
+  Camera,
+  Menu,
+  Notification,
+  Search,
+} from "../../../shared/ui";
+
+interface HeaderProps {
+  className?: string;
+  onToggleSidebar: () => void;
+}
+
+export const Header: React.FC<HeaderProps> = ({
+  className,
+  onToggleSidebar,
+}) => {
+  return (
+    <header className={`${styles.header} ${className}`}>
+      <div className={styles["header-left"]}>
+        <button className={styles.menu} onClick={onToggleSidebar}>
+          <Burger />
+        </button>
+        <a href="#" className={styles.logo}>
+          <img src="http://localhost:5173/assets/logo.png" alt="logo" />
+        </a>
+        <label htmlFor="search-input" className={styles.search}>
+          <input
+            type="text"
+            className={styles["search-input"]}
+            id="search-input"
+            placeholder="Search"
+          />
+          <div className={styles["search-icon"]}>
+            <Search />
+          </div>
+        </label>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.icons}>
+          <Button variant="icon">
+            <Camera />
+          </Button>
+          <Button variant="icon">
+            <Menu />
+          </Button>
+          <Button className={styles.notifications} variant="icon">
+            <span className={styles["notifications-count"]}>3</span>
+            <Notification />
+          </Button>
+        </div>
+        <Avatar
+          size="md"
+          avatarUrl="http://localhost:5173/assets/avatars/avatar-1.png"
+        />
+      </div>
+    </header>
+  );
+};
