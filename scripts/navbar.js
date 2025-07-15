109fb337d506ca7619756d31932a13cf6310c7ac
vladislav.aleynikov <vladislav.aleinikov@innowise.com>
2025-07-15 08:30:03 +0300

    fix navbar popup

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,7 +1,7 @@
-const navbarList = document.querySelector(".navbar__list");
+const navbarPopup = document.querySelector(".navbar__popup");
 const navbarShowButton = document.querySelector(".navbar__show-more");
 
 navbarShowButton.addEventListener("click", (e) => {
-  navbarList.classList.toggle("navbar__list--show");
+  navbarPopup.classList.toggle("navbar__popup--show");
   navbarShowButton.classList.toggle("navbar__show-more--active")
 })
